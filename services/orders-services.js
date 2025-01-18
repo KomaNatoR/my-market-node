fc09ea923617fce0ba4ffcad72c22f2d20db6bcb
MarkovSerhii <goo.komalel@gmail.com>
2025-01-18 18:56:59 +0200

    some add-order work!

--- services/orders-services.js
+++ services/orders-services.js
@@ -8,24 +8,29 @@ const ordersPath = path.join(__dirname, "../db/chese-market/orders.json");
 
 const getAll = async () => {
   const orders = await fs.readFile(ordersPath, "utf-8");
+  
   return JSON.parse(orders);
 }
 const add = async ({ name, phone, items }) => {
-  const orders = await getAll();
+  const total = items.reduce((sum, { quantity, price }) => sum + quantity * price, 0);
+  
   const newOrder = {
     id: nanoid(),
     name,
     phone,
     items,
-    date: new Date().toISOString(),
+    date: new Date().toLocaleString(),
+    total,
     status: "pending",
     delivery: {"method": "courier", "address": "Vinnytsia, Main St. 10"}
-  };
+  }
+
+  const orders = await getAll();
   orders.push(newOrder);
-  await fs.writeFile(ordersPath, JSON.stringify(contacts, null, 2));
+  await fs.writeFile(ordersPath, JSON.stringify(orders, null, 2));
+
   return newOrder;
 }
-console.log(__dirname);
 
 
 module.exports = {
