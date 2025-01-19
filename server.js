099c289a82cdf23741a568f1a9aeeb5b50a93b40
MarkovSerhii <goo.komalel@gmail.com>
2025-01-19 19:35:00 +0200

    move DB_HOST  to proces.env!

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-// "1241Mon_Mon1241"
+const { DB_HOST } = process.env;
 
 
-const DB_HOST = "mongodb+srv://Serhii:1241Mon_Mon1241@cluster0.gznqb.mongodb.net/market?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(DB_HOST)
     .then(app.listen(3000, () => console.log("SERVING! Port:3000")))
     .catch(error => console.log(error.message));
